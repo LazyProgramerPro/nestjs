1c9688829884a8972110aade298dad8fb4ea7ee6
[BK]Phù Dung <thuongtt@edsolabs.com>
2022-03-18 16:09:38 +0700

    cau hinh data base voi prisma

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [PrismaModule],
+  // khi su dung 2 module o ngon thi phai exp ra ngoai trong file module sau do import
+  //  vaof cac module khac
+  //  (VD: exp Servie prisma ==> modulePrimsa==>import vao AUthModule==>dung o AUTH Servic)
   controllers: [AuthController],
   providers: [AuthService],
 })
