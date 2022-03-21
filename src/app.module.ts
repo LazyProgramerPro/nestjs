64a74d3aec0d13b31ed4df0fdb77dc036ebcb825
[BK]Phù Dung <thuongtt@edsolabs.com>
2022-03-21 18:34:14 +0700

    add module by cmd

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     BookmarkModule,
     PrismaModule,
+    CatsModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,15 @@ async function bootstrap() {
       whitelist: true,
     })
   ); //su dung de bat validate
+  const options = new DocumentBuilder()
+    .setTitle('Bookmark example')
+    .setDescription('The Bookmark API description')
+    .setVersion('1.0')
+    .addTag('bookmarks')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3333);
 }
 bootstrap();
